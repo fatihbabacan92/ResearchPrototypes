ed4197c4906445c5c85f14991344ad1e9d693ea4
Fatih <fatihb@live.be>
2020-05-24 21:06:59 +0100

    Applied Font Format Rules

--- XceedWords/XceedWords/Program.cs
+++ XceedWords/XceedWords/Program.cs
@@ -1,4 +1,5 @@
-using System;
+﻿using System;
+using System.Drawing;
 using System.IO;
 using Xceed.Document.NET;
 using Xceed.Words.NET;
@@ -9,12 +10,20 @@ namespace XceedWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //find and replace text
+            //Apply format rules
+
             //Find File
             string filename = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             filename = Path.Combine(filename + @"\WordFile\test.docx");
+
             //Load Document -- DocX.Create() for creating new document
             var document = DocX.Load(filename);
+
+            //Set Default Font to Times New Roman, 12px, Black
+            document.SetDefaultFont(new Font("Times New Roman"), 12d, Color.Black);
+
+
         }
     }
 }
