ac9eb40d02747fe86e1c4655dc3b75c9c1a997e2
Fatih <fatihb@live.be>
2020-05-26 17:58:29 +0100

    Added FindFile to XML

--- OpenXML/OpenXML/Program.cs
+++ OpenXML/OpenXML/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace OpenXML
 {
@@ -6,7 +6,15 @@ namespace OpenXML
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Processing document");
+
+            //Find File
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string filepath = Path.Combine(desktopPath + @"\WordFile\test_xml.docx");
+            //Save Original File
+            string originalFilename = Path.GetFileNameWithoutExtension(filepath);
+            var originalFile = Path.Combine(desktopPath, @"WordFile\" + originalFilename + "-v1.docx");
+            System.IO.File.Copy(filepath, originalFile);
         }
     }
 }
