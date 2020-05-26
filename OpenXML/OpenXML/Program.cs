f45ca57240110626b57ab964ca91153286e66158
Fatih <fatihb@live.be>
2020-05-26 21:55:31 +0100

    Added Paragraph Style to XML

--- OpenXML/OpenXML/Program.cs
+++ OpenXML/OpenXML/Program.cs
@@ -1,4 +1,8 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Bibliography;
 using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using DocumentFormat.OpenXml.Office2010.Word;
+using DocumentFormat.OpenXml.Office2013.Word;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System;
@@ -6,6 +10,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Color = DocumentFormat.OpenXml.Wordprocessing.Color;
 
 namespace OpenXML
 {
@@ -52,6 +57,8 @@ namespace OpenXML
             Run run = titleParagraph.AppendChild(new Run());
             run.AppendChild(new Text(correctedTitle));
 
+            //Apply Paragraph Style
+            ApplyStyleToParagraph(doc, "H1", "Heading1", titleParagraph);
 
             //Close and Save File
             doc.Save();
@@ -59,6 +66,119 @@ namespace OpenXML
 
             Console.WriteLine("Done");
             Console.ReadLine();
+        }
+
+        public static void ApplyStyleToParagraph(WordprocessingDocument doc, string styleid, string stylename, Paragraph p)
+        {
+            if (p.Elements<ParagraphProperties>().Count() == 0)
+            {
+                p.PrependChild<ParagraphProperties>(new ParagraphProperties());
+            }
+
+            ParagraphProperties pPr = p.Elements<ParagraphProperties>().First();
+
+            StyleDefinitionsPart part =
+                doc.MainDocumentPart.StyleDefinitionsPart;
+
+            if (part == null)
+            {
+                part = AddStylesPartToPackage(doc);
+                AddNewStyle(part, styleid, stylename);
+            }
+            else
+            {
+                if (IsStyleIdInDocument(doc, styleid) != true)
+                {
+                    string styleidFromName = GetStyleIdFromStyleName(doc, stylename);
+                    if (styleidFromName == null)
+                    {
+                        AddNewStyle(part, styleid, stylename);
+                    }
+                    else
+                        styleid = styleidFromName;
+                }
+            }
+
+            pPr.ParagraphStyleId = new ParagraphStyleId() { Val = styleid };
+        }
+
+        public static bool IsStyleIdInDocument(WordprocessingDocument doc,
+            string styleid)
+        {
+            Styles s = doc.MainDocumentPart.StyleDefinitionsPart.Styles;
+
+            int n = s.Elements<Style>().Count();
+            if (n == 0)
+                return false;
+
+            Style style = s.Elements<Style>()
+                .Where(st => (st.StyleId == styleid) && (st.Type == StyleValues.Paragraph))
+                .FirstOrDefault();
+            if (style == null)
+                return false;
+
+            return true;
+        }
+
+        public static string GetStyleIdFromStyleName(WordprocessingDocument doc, string styleName)
+        {
+            StyleDefinitionsPart stylePart = doc.MainDocumentPart.StyleDefinitionsPart;
+            string styleId = stylePart.Styles.Descendants<StyleName>()
+                .Where(s => s.Val.Value.Equals(styleName) &&
+                    (((Style)s.Parent).Type == StyleValues.Paragraph))
+                .Select(n => ((Style)n.Parent).StyleId).FirstOrDefault();
+            return styleId;
+        }
+
+        private static void AddNewStyle(StyleDefinitionsPart styleDefinitionsPart,
+            string styleid, string stylename)
+        {
+            Styles styles = styleDefinitionsPart.Styles;
+
+            Style style = new Style()
+            {
+                Type = StyleValues.Paragraph,
+                StyleId = styleid,
+                CustomStyle = true
+            };
+            StyleName styleName1 = new StyleName() { Val = stylename };
+            BasedOn basedOn1 = new BasedOn() { Val = "Normal" };
+            NextParagraphStyle nextParagraphStyle1 = new NextParagraphStyle() { Val = "Normal" };
+            style.Append(styleName1);
+            style.Append(basedOn1);
+            style.Append(nextParagraphStyle1);
+
+            StyleRunProperties styleRunProperties1 = new StyleRunProperties();
+            Bold bold1 = new Bold();
+            Color color1 = new Color() { Val = "000000" };
+            RunFonts font1 = new RunFonts() { Ascii = "Times New Roman" };
+            // Specify a 12 point size.
+            FontSize fontSize1 = new FontSize() { Val = "64" };
+
+            styleRunProperties1.Append(bold1);
+            styleRunProperties1.Append(color1);
+            styleRunProperties1.Append(font1);
+            styleRunProperties1.Append(fontSize1);
+
+            ParagraphProperties paragraphProperties = new ParagraphProperties();
+            Justification centerHeading = new Justification() { Val = JustificationValues.Center };
+            OnOffValue hyphenOff = new OnOffValue(false);
+            AutoHyphenation hyphenation = new AutoHyphenation() { Val =  hyphenOff };
+            paragraphProperties.Append(centerHeading);
+            paragraphProperties.Append(hyphenation);
+
+            style.Append(styleRunProperties1);
+            styles.Append(style);
+            styles.Append(paragraphProperties);
+        }
+
+        public static StyleDefinitionsPart AddStylesPartToPackage(WordprocessingDocument doc)
+        {
+            StyleDefinitionsPart part;
+            part = doc.MainDocumentPart.AddNewPart<StyleDefinitionsPart>();
+            Styles root = new Styles();
+            root.Save(part);
+            return part;
         }
     }
 }
